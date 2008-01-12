8f32bd7ed92c298b58a3cff310a77f77f893de87
pfpschneider <pfpschneider@c762a732-0a2c-0410-af75-4186e90c83c4>
2008-01-12 13:29:48 +0000

    Editing OWL Primer document

--- owl/documents/dl-tabbed.js
+++ owl/documents/dl-tabbed.js
@@ -9,7 +9,8 @@
   */
  
 // Hide dd only in DOM-capable browsers.
-if (document.getElementsByTagName) document.write("<style type='text/css'>dl.tabbed dd {display: none;}</style>");
+if (document.getElementsByTagName)
+  document.write("<style type='text/css'>dl.tabbed dd {display: none;}</style>");
 
 var tabbedDL = {
  item : 0, // define the first displayed definition description
@@ -36,7 +37,9 @@ var tabbedDL = {
   el = dl.getElementsByTagName("DT");
   for (var i = 0; i < el.length; i++) {
    if (!el[i].getElementsByTagName("A")[0]){
-    el[i].innerHTML = "<a " + tabbedDL.event + "='tabbedDL.showDD(this);' href='javascript:;'>" + el[i].innerHTML + "</a>";
+    el[i].innerHTML = "<a " + tabbedDL.event +
+		           "='tabbedDL.showDD(this);' href='javascript:;'>"
+			   + el[i].innerHTML + "</a>";
     el[i].className = "";
     }
    }
@@ -58,6 +61,7 @@ var oldonload=window.onload;if(typeof window.onload!='function'){
 window.onload=tabbedDL.setup;
 }else{window.onload=function(){oldonload();
 tabbedDL.setup();}}
+
   /* setup faster by deleting these lines and adding
          <script type="text/javascript">tabbedDL.setup();</script>
          before the closing body tag in your HTML */
